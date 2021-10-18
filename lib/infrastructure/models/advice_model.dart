c608a30dc670cfee1e5cc15cbbcb9c49cc8bd36a
Marco Seraphin <seraphin@develope-it.de>
2021-10-18 15:29:44 +0200

    first checkin

--- lib/infrastructure/models/advice_model.dart
+++ lib/infrastructure/models/advice_model.dart
@@ -4,6 +4,7 @@ class AdviceModel extends AdviceEntity {
   AdviceModel({required String advice, required int id})
       : super(advice: advice, id: id);
 
+  /// Factory Constructor
   factory AdviceModel.fromJson(Map<String, dynamic> json) {
     return AdviceModel(advice: json["advice"], id: json["id"]);
   }
